51e3d3f73ed90e62b790ee117e2649a7a76ba78e
Victor L. <johnsleeder1@gmail.com>
2026-01-05 19:21:54 -0300

    Add mainteste.js with execution and processing functions

---
+++ 5. Sistema de Extração/src/mainteste.js
@@ -0,0 +1,18 @@
+// Projeto demonstrativo – JavaScript
+// Código adaptado exclusivamente para portfólio
+
+function executar() {
+  console.log("Execução iniciada");
+  processar();
+  finalizar();
+}
+
+function processar() {
+  for (let i = 1; i <= 5; i++) {
+    console.log(`Processando item ${i}`);
+  }
+}
+
+function finalizar() {
+  console.log("Processo finalizado");
+}
